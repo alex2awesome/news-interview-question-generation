b500e3edd7f321c5f2a492e88d96e0a98477628f
Michael Lu <michael.lu@berkeley.edu>
2024-09-30 19:12:23 -0400

    update

---
+++ sbatch-scripts/game_sim/generate_info_items_mit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=8:00:00
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=100
+#SBATCH --mem=400G
+#SBATCH --partition=sched_mit_psfc_gpu_r8
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=michael.lu@berkeley.edu
+
+cd /pool001/spangher/alex/news-interview-question-generation
+source env_setup.sh
+export OMP_NUM_THREADS=50
+
+python -m game_sim.data_processing.run_all_mit

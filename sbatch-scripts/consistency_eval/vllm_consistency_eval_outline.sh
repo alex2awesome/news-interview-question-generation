a40017404cabafe684ffd3e55f2de7e74e7e52df
Alex Spangher <spangher@usc.edu>
2024-10-13 04:13:27 -0700

    updated

---
+++ sbatch-scripts/consistency_eval/vllm_consistency_eval_outline.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=40:00:00
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-gpu=5
+#SBATCH --mem=150G
+#SBATCH --partition=isi
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-user=michael.lu@berkeley.edu
+
+cd /project/jonmay_231/spangher/Projects/news-interview-question-generation
+
+source env_setup.sh
+
+python -m evaluators.vllm_consistency_eval \
+    --dataset_path /project/jonmay_1426/spangher/news-interview-question-generation/output_results/outline/QA_Seq_LLM_generated.csv \
+    --output_dir /project/jonmay_1426/spangher/news-interview-question-generation/output_results/consistency_eval_outline
